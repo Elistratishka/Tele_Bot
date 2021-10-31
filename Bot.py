0f96b5021662c251d2e47840f0a8ceda211b0d86
Elistratishka <elistratishka@outlook.com>
2021-10-31 12:58:42 +0300

    №1

---
+++ Bot.py
@@ -0,0 +1,47 @@
+import os
+import telebot
+from dotenv import load_dotenv
+from extensions import get_data, convert, ConvertionException
+
+load_dotenv()
+token = os.getenv("TOKEN")
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help_(message: telebot.types.Message):
+    rules = "Бот применим для конвертации стоимости инстранных валют и рубля по курсу ЦБ на сегодня, " \
+            "а также для конвертации стоимости токенов с площадки alcor exchange\n" \
+            "Для использования бота напишите ему сообщение в формате:\n <количество валюты>" \
+            " <валюта из которой переводить> <валюта в которую переводить>\n" \
+            "Введите команду /values для получения информации о доступных для конвертации валютах и токенах"
+    bot.reply_to(message, rules)
+
+
+@bot.message_handler(commands=['values'])
+def start(message: telebot.types.Message):
+    text = 'Доступные валюты: '
+    data = get_data()
+    for values in data.keys():
+        text = '\n'.join((text, values,))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def work(message: telebot.types.Message):
+    try:
+        letter = message.text.replace(',', '.').upper().split(' ')
+        if len(letter) != 3:
+            raise ConvertionException("Введено неверное количество параметров.")
+        amount, quote, base = letter
+        cost = convert(amount, quote, base)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка: {e}')
+    except Exception:
+        bot.send_message(message.chat.id, 'Извините что-то пошло не так')
+    else:
+        text = f'Стоимость {amount} {quote} в {base} = {cost}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.infinity_polling()
